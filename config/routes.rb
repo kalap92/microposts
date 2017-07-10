c700929b802bbfb9cc60167e940129b84b8a6634
kalap92 <piotrkala92@gmail.com>
2017-07-10 20:09:10 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
+  get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 
   resources :microposts
-  root 'application#hello'
-
   resources :users
 end
